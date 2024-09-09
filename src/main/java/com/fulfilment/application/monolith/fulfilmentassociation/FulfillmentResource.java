5ed9a0f939f45307b3942b0d6fb54c9e2a452e74
Udayanga.Gamage <ugamage85@gmail.com>
2024-09-09 02:06:40 +0200

    code refactoring

---
+++ src/main/java/com/fulfilment/application/monolith/fulfilmentassociation/FulfillmentResource.java
@@ -0,0 +1,39 @@
+package com.fulfilment.application.monolith.fulfilmentassociation;
+
+import com.fulfilment.application.monolith.fulfilmentassociation.domain.Fulfillment;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+import java.util.List;
+
+@Path("/fulfillments")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class FulfillmentResource {
+    @Inject
+    FulfillmentService fulfillmentService;
+
+    @POST
+    public Response createFulfillment(FulfillmentRequest request) {
+        Fulfillment fulfillment = toModel(request);
+        FulfillmentAssociation fulfillmentAssociation = fulfillmentService.createFulfillment(fulfillment);
+        return Response.status(Response.Status.CREATED).entity(fulfillment).build();
+    }
+
+    @GET
+    public List<FulfillmentAssociation> getFulfillments(@QueryParam("storeId") Long storeId,
+                                                        @QueryParam("productId") Long productId) {
+        return fulfillmentService.getFulfillments(storeId, productId);
+    }
+
+    private Fulfillment toModel(FulfillmentRequest request) {
+        return new Fulfillment(request.getStoreId(), request.getProductId(), request.getWarehouseIds());
+    }
+}
---
+++ src/main/java/com/fulfilment/application/monolith/fulfilmentassociation/FulfillmentService.java
@@ -0,0 +1,120 @@
+package com.fulfilment.application.monolith.fulfilmentassociation;
+
+import com.fulfilment.application.monolith.exceptions.ErrorRule;
+import com.fulfilment.application.monolith.exceptions.FulfillmentException;
+import com.fulfilment.application.monolith.fulfilmentassociation.domain.Fulfillment;
+import com.fulfilment.application.monolith.products.Product;
+import com.fulfilment.application.monolith.products.ProductRepository;
+import com.fulfilment.application.monolith.stores.Store;
+import com.fulfilment.application.monolith.warehouses.adapters.database.DbWarehouse;
+import com.fulfilment.application.monolith.warehouses.adapters.database.WarehouseRepository;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@ApplicationScoped
+public class FulfillmentService {
+
+    @Inject
+    FulfillmentRepository fulfillmentRepository;
+
+    @Inject
+    WarehouseRepository warehouseRepository;
+
+    @Inject
+    ProductRepository productRepository;
+
+    @Transactional
+    public FulfillmentAssociation createFulfillment(Fulfillment fulfillment) {
+        Long storeId = fulfillment.getStoreId();
+        Long productId = fulfillment.getProductId();
+        List<Long> warehouseIds = fulfillment.getWarehouseIds();
+        Store store = Store.findById(storeId);
+        Product product = productRepository.findById(productId);
+        List<DbWarehouse> warehouses = warehouseRepository.list("id IN ?1", warehouseIds);
+
+        if (isFulfillmentDuplicate(storeId, productId, warehouseIds)) {
+            throw new FulfillmentException(ErrorRule.FULFILLMENT_ALREADY_EXIST);
+        }
+
+
+        validateProductFulfillment(storeId, productId, warehouseIds);
+        validateStoreFulfillment(storeId, warehouseIds);
+        validateWarehouseFulfillment(warehouseIds);
+
+       /* if (warehouses.size() > 2) {
+            throw new IllegalStateException("A product can be fulfilled by up to 2 warehouses per store.");
+        }
+
+        if (fulfillmentRepository.countByStore(storeId) >= 3) {
+            throw new IllegalStateException("A store can be fulfilled by up to 3 warehouses.");
+        }
+
+        for (DbWarehouse warehouse : warehouses) {
+            if (warehouseRepository.countProductsInWarehouse(warehouse.id) >= 5) {
+                throw new IllegalStateException("Warehouse can store up to 5 types of products.");
+            }
+        }*/
+
+        FulfillmentAssociation fulfillmentAssociation = new FulfillmentAssociation();
+        fulfillmentAssociation.setStore(store);
+        fulfillmentAssociation.setProduct(product);
+        fulfillmentAssociation.setWarehouses(warehouses);
+
+        fulfillmentRepository.persist(fulfillmentAssociation);
+
+        return fulfillmentAssociation;
+    }
+
+    public List<FulfillmentAssociation> getFulfillments(Long storeId, Long productId) {
+        if (storeId != null && productId != null) {
+            return fulfillmentRepository.findByStoreAndProduct(storeId, productId);
+        } else {
+            return fulfillmentRepository.listAll();
+        }
+    }
+    private void validateProductFulfillment(Long storeId, Long productId, List<Long> warehouseIds) {
+        var uniqueWarehouseIds = getUniqueWarehouseIds(warehouseIds);
+        List<FulfillmentAssociation> existingFulfillments = fulfillmentRepository.findByStoreAndProduct(storeId, productId);
+        if (existingFulfillments.size() + uniqueWarehouseIds.size() > 2) {
+            throw new FulfillmentException(ErrorRule.PRODUCT_FULFILMENT_MAX_WAREHOUSES_PER_STORE_EXCEEDED);
+        }
+    }
+
+    private void validateStoreFulfillment(Long storeId, List<Long> warehouseIds) {
+        var uniqueWarehouseIds = getUniqueWarehouseIds(warehouseIds);
+        if (fulfillmentRepository.countByStore(storeId) + uniqueWarehouseIds.size() > 3) {
+            throw new FulfillmentException(ErrorRule.STORE_FULFILMENT_MAX_WAREHOUSES_EXCEEDED);
+        }
+    }
+
+    private void validateWarehouseFulfillment(List<Long> warehouseIds) {
+        var uniqueWarehouseIds = getUniqueWarehouseIds(warehouseIds);
+        for (Long warehouseId : uniqueWarehouseIds) {
+            if (fulfillmentRepository.countProductsInWarehouse(warehouseId) >= 5) {
+                throw new FulfillmentException(ErrorRule.WAREHOUSE_MAX_PRODUCT_TYPES_EXCEEDED);
+            }
+        }
+    }
+
+    private Set<Long> getUniqueWarehouseIds(List<Long> warehouseIds) {
+        Set<Long> uniqueWarehouseIds = new HashSet<>(warehouseIds);
+        //return uniqueWarehouseIds.size() == warehouseIds.size();
+        return uniqueWarehouseIds;
+    }
+
+    private boolean isFulfillmentDuplicate(Long storeId, Long productId, List<Long> warehouseIds) {
+        var uniqueWarehouseIds = getUniqueWarehouseIds(warehouseIds);
+        List<FulfillmentAssociation> existingFulfillments = fulfillmentRepository.findByStoreAndProduct(storeId, productId);
+        for (FulfillmentAssociation existingFulfillment : existingFulfillments) {
+            if (existingFulfillment.getWarehouses().stream().map(DbWarehouse::getId).toList().containsAll(uniqueWarehouseIds)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
